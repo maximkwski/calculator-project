51f2246bf177d14cbb5e584feecad8cc89e801c5
Maksym <makkie.makowski@gmail.com>
2022-11-28 16:48:06 -0500

    fix add function with multiple operands

--- script.js
+++ script.js
@@ -4,27 +4,32 @@ let result = null;
 let operator = "";
 
 function add(a,b) {
-    return a + b;
+    return Number(a) + Number(b);
 };
 
 function substract(a,b) {
-    return a - b;
+    return Number(a) - Number(b)
 };
 
-function multiply (a,b) {
-    return a * b;
+function multiply(a,b) {
+    return Number(a) * Number(b)
 };
 
 function devide(a,b) {
-    return a / b;
+    return Number(a) / Number(b)
 };
 
 function operate(operator,a,b) {
     switch (operator) {
-        case "add" : result = add(a,b); break ;
-        case "substract" : result = substract(a,b); break ;
-        case "multiply" : result = multiply(a,b); break ;
-        case "devide" : result = devide(a,b); break ;
+        case "add" : result = add(a,b);
+
+        break ;
+        case "substract" : result = substract(a,b); 
+        break ;
+        case "multiply" : result = multiply(a,b); 
+        break ;
+        case "devide" : result = devide(a,b); 
+        break ;
     }
 };
 
@@ -33,13 +38,26 @@ const digitBtn = Array.from(document.querySelectorAll('.digits button'));
 const operatorBtn = Array.from(document.querySelectorAll('.operators button'));
 let operatorCalled = false;
 console.log(operatorCalled);
+
 operatorBtn.forEach(btn => {
     btn.addEventListener('click', () => {
     if(btn.textContent != "="){
         operatorCalled = true;
-        display.textContent = null;
+        // display.textContent = null;
         if (btn.textContent === "+") { 
             operator = "add";
+            if (b != null) {
+                operate(operator,a,b);
+                console.log(operator,a,b);
+                console.log("result for now is :", result);
+                a = result;
+                b = null;
+                display.textContent = a;
+
+            }
+            
+            
+
         } else if (btn.textContent === "-") {
             operator = "substract";
         } else if (btn.textContent === "*" ){
@@ -51,23 +69,31 @@ operatorBtn.forEach(btn => {
     } else {
         operate(operator,a,b);
         console.log(operator,a,b);
-        console.log(result);
+    
         display.textContent = result;
     }
     });
     
 });
 
-
 function populateDisplay() {
     digitBtn.forEach(btn => {
         btn.addEventListener("click", () => {
             if (operatorCalled === false) {
-              a = display.textContent += btn.textContent;
+                if (display.textContent == 0) {
+                    display.textContent = null;
+                }
+                a = display.textContent += btn.textContent;
               console.log('value of A is: ',a);
             } else {
+                if (display.textContent != null) {
+                    display.textContent = b;
+                }
                 b = display.textContent += btn.textContent;
+
+                console.log('value of a is: ',a);
                 console.log('value of B is: ',b);
+                
             }
         });
 
